8708fdea7603a5b108d9a507e0b6c53ad6237323
Bilal-motiwala <bilalmotiwala20@gmail.com>
2024-12-04 23:17:48 +0500

    Added Header

---
+++ Components/Fashion.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Fashion = () => {
+  return (
+    <main className='w-[217px] h-[142px] left-[368px] rounded-lg opacity-[30%] text-[#000000] text-center'>
+        
+        {/* Top 1 */}
+
+      <div className='w-[142px] h-[24px] font-light poppins size-[16px] leading-[24px] text-center text-[#000000] mt-4 ml-80 '>
+        Woman's Fashion
+      </div>
+
+      <div className='w-[217px] h-[24px] gap-[81px]'>
+        <div className='w-[112px] h-[24px] poppins font-normal size-[16px] text-center leading-[24px] text-[#000000] mt-4 ml-[150%]'>
+             Men's Fashion
+        </div>
+      </div>
+
+      <div className='w-[87px] h-[24px] poppins font-normal size-[16px] leading-[24px] text-center text-[#000000] mt-4 ml-80 '>
+        Electronic
+      </div>
+
+      <div className='w-[132px] h-[24px] poppins font-normal size-[16px] leading-[24px] text-center text-[#000000] mt-4 ml-[150%] '>
+        Home & Lifestyle 
+      </div>
+
+      <div className='w-[73px] h-[24px] poppins font-normal size-[16px] leading-[24px] text-center text-[#000000] mt-4 ml-[150%] '>
+        Medicine
+      </div>
+
+      <div className='w-[137px] h-[24px] poppins font-normal size-[16px] leading-[24px] text-center text-[#000000] mt-4 ml-80 '>
+        Sports & Outdoor
+      </div>
+
+      <div className='w-[109px] h-[24px] poppins font-normal size-[16px] leading-[24px] text-center text-[#000000] mt-4 ml-80 '>
+        Baby's & Toys
+      </div>
+
+      <div className='w-[131px] h-[24px] poppins font-normal size-[16px] leading-[24px] text-center text-[#000000] mt-4 ml-80 '>
+        Groceries & Pets 
+      </div>
+
+      <div className='w-[128px] h-[24px] poppins font-normal size-[16px] leading-[24px] text-center text-[#000000] mt-4 ml-80 '>
+        Health & Beauty
+      </div>
+    </main>
+  )
+}
+
+export default Fashion
---
+++ page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Header from "./Components/Header";
+import Fashion from "./Components/Fashion";
+export default function Home() {
+  return (
+    <>
+    <Header />
+    <Fashion />
+    </>
+     );
+}
